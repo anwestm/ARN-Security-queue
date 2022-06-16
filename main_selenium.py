de406d85de53f9ee2fe570d85a10816ac277502f
Andreas Westman <anwestm@kth.se>
2022-06-16 11:08:56 +0200

    moved parser logic into separate file

--- main_selenium.py
+++ main_selenium.py
@@ -3,7 +3,17 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from pyvirtualdisplay import Display
 
+import schedule
+import time
+import datetime
 import scraper
+
+def load_site():
+    print("Loading webpage...")
+    driver.get('https://www.swedavia.se/arlanda/sakerhetskontroll/')
+
+    print("Parsing html...")
+    scraper.parse_tree(driver.page_source)
 
 print("Creating display driver object...")
 display = Display(visible=0, size=(800, 600))
@@ -14,9 +24,10 @@ display.start()
 print("Loading Chrome...")
 driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
-print("Loading webpage...")
-driver.get('https://www.swedavia.se/arlanda/sakerhetskontroll/')
+schedule.every(5).minutes.do(load_site)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
-print("Parsing html...")
-scraper.parse_tree(driver.page_source)
+
 driver.quit()
