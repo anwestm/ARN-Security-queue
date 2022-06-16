654f1c60ead265d9fd9b654d5d7b6bae7f278ecc
Andreas <anwestm@kth.se>
2022-06-16 12:06:41 +0200

    fix

--- main_selenium.py
+++ main_selenium.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from pyvirtualdisplay import Display
 
@@ -8,12 +9,14 @@ import time
 import datetime
 import scraper
 
-def load_site():
-    print("Loading webpage...")
-    driver.get('https://www.swedavia.se/arlanda/sakerhetskontroll/')
+def reload_site():
+    start = time.time()
+    print("Reloading webpage...")
+    driver.refresh()
 
     print("Parsing html...")
     scraper.parse_tree(driver.page_source)
+    print("Parsing complete after", str(time.time()-start), "seconds.")
 
 print("Creating display driver object...")
 display = Display(visible=0, size=(800, 600))
@@ -21,10 +24,24 @@ display = Display(visible=0, size=(800, 600))
 print("Starting driver...")
 display.start()
 
-print("Loading Chrome...")
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+options = Options()
+options.add_argument("disable-infobars")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 
-schedule.every(5).minutes.do(load_site)
+print("Loading Chrome...")
+driver = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver", chrome_options=options)
+
+print("Loading webpage...")
+driver.get("https://www.swedavia.se/arlanda/sakerhetskontroll/")
+
+print("Parsing html...")
+scraper.parse_tree(driver.page_source)
+
+schedule.every(5).minutes.do(reload_site)
 while True:
     schedule.run_pending()
     time.sleep(1)
