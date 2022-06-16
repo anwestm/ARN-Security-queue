e0fd6fda5d0d66ab000253cc9fb3381e65be5327
Andreas <anwestm@kth.se>
2022-06-16 11:03:14 +0200

    fixed selenium driver

--- main_selenium.py
+++ main_selenium.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from pyvirtualdisplay import Display
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+import scraper
+
+print("Creating display driver object...")
+display = Display(visible=0, size=(800, 600))
+
+print("Starting driver...")
+display.start()
+
+print("Loading Chrome...")
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+
+print("Loading webpage...")
 driver.get('https://www.swedavia.se/arlanda/sakerhetskontroll/')
-print(driver.page_source)
+
+print("Parsing html...")
+scraper.parse_tree(driver.page_source)
 driver.quit()
